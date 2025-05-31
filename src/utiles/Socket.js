b85734fc2d022da17b6cc48c0cc1d33588cfb9c9
Jeeveshmahato <jeeveshmahato100@gmail.com>
2025-06-01 02:12:46 +0530

    S-2

--- src/utiles/Socket.js
+++ src/utiles/Socket.js
@@ -1,14 +1,30 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
 const initailizeSocket = (server) => {
+  const getSecretRoomId = (userId, targetUserId) => {
+    return crypto
+      .createHash("sha256")
+      .update([userId, targetUserId].sort().join("$"))
+      .digest("hex");
+  };
   const io = socket(server, {
     cors: {
       origin: "http://localhost:5173",
     },
   });
-  io.on("connection",(socket)=>{
-    socket.on("joinChat",()=>{})
-    socket.on("sendMessage",()=>{})
-    socket.on("disconnect",()=>{})
-  } )
+  io.on("connection", (socket) => {
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      const roomId = getSecretRoomId(userId, targetUserId);
+      console.log(firstName + "joined Room : " + roomId);
+      console.log(roomId);
+    });
+    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+      const roomId = getSecretRoomId(userId, targetUserId);
+      console.log(firstName + " " + text);
+      io.to(roomId).emit("messageReceived", { firstName, text });
+    });
+    socket.on("disconnect", () => {});
+  });
 };
-module.exports = initailizeSocket
+module.exports = initailizeSocket;
